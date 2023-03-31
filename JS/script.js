c7b3369d6f53071fd57a907372d30586f48d133f
andreaconv <andrea2convertini@gmail.com>
2023-03-31 18:35:15 +0200

    [add] bozza di controllo

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,47 @@
-
+//prendo gli elementi dall'HTML
 const h1 = document.querySelector("h1");
-console.log(h1);
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
 //creo la lista da mostrare
 let list = [];
+//creo la lista dei numeri inseriti
+let numberInput = [];
+console.log(numberInput)
 
-for (let i = 0; i < 5; i++) {
+
+//ciclo per generare i numeri random
+for (let i = 0; i < 1; i++) {
   const numeroRandom = generateRandomNumber(list, 1, 100);
   list.push(numeroRandom);
 }
-console.log(list);
+console.log(list)
 //inserisco la lista nell'HTML
 h1.innerHTML = list;
 
+//dopo 5 secondo elimino la lista e faccio apparire l'input e il bottone
 setTimeout(function () {
-  h1.className = ("hide");
-}, 5000)
+  h1.classList.add("hide");
+  input.classList.remove("hide");
+  btn.classList.remove("hide");
+}, 1000)
+
+//TODO:prima di prendere i valori inseriti devo controllare che siano giusti (dei numeri)
+
+
+//prendo i valori inseriti e li confronto con la lista dei numeri
+//se sono tutti esatti output giusto altrimenti riprova
+btn.addEventListener("click", function(){
+  numberInput.push(input.value);
+
+  if (list.includes(numberInput)){
+    console.log("sbagliato")
+  }else{
+    console.warn("giusto")
+  }
+
+  
+})
+
 
 
 
